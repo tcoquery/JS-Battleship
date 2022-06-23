a5917dec81a3e03c14e937ccff64c854ed9bdcbb
tcoquery <tcoquery@gmail.com>
2022-06-23 17:42:19 +0200

    Add gameboard module

--- src/index.js
+++ src/index.js
@@ -2,4 +2,35 @@ import { ship } from './modules/ship';
 import { gameboard } from './modules/gameboards';
 
 const board = gameboard();
-board.createGrid();
+const vertBtn = document.querySelector('.vert');
+const horiBtn = document.querySelector('.hori');
+let orientation = '';
+
+function createGrid() {
+  for (let i = 0; i < 100; i++) {
+    const grid = document.querySelector('.grid');
+    const gridCell = document.createElement('div');
+    gridCell.classList.add('grid-cell');
+    gridCell.id = i;
+    grid.appendChild(gridCell);
+  }
+}
+
+createGrid();
+
+const gridCells = document.querySelectorAll('.grid-cell');
+
+gridCells.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    board.placeShip(parseInt(cell.id), 5, orientation);
+    console.log(board.grid);
+  });
+});
+
+vertBtn.addEventListener('click', () => {
+  orientation = 'vert';
+});
+
+horiBtn.addEventListener('click', () => {
+  orientation = 'hori';
+});
--- src/modules/gameboards.js
+++ src/modules/gameboards.js
@@ -1,15 +1,52 @@
 const gameboard = () => {
-  const grid = document.querySelector('.grid');
+  const grid = [
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+  ];
 
-  function createGrid() {
-    for (let i = 0; i < 100; i += 1) {
-      const gridCell = document.createElement('div');
-      gridCell.classList.add('grid-cell');
-      gridCell.id = i;
-      grid.appendChild(gridCell);
+  function placeShip(position, length, orientation) {
+    if (orientation == 'hori') {
+      grid[position] = 'S';
+      for (let i = 1; i < length; i++) {
+        grid[position + i] = 'S';
+      }
+    } else if (orientation == 'vert') {
+      grid[position] = 'S';
+      for (let i = 10; i < (length * 10); i += 10) {
+        grid[position + i] = 'S';
+      }
     }
+  }
+
+  function receiveAttack(position, ship) {
+    if (grid[position]) {
+      ship.hit();
+    } else {
+      grid[position] = 'O';
+    }
+  }
+
+  const shipPresent = (element) => element == 'S';
+
+  function shipsSunk() {
+    if (grid.some(shipPresent)) {
+      return
+    } else {
+      console.log('game over');
+    }
+  }
+
+  return {
+    grid, placeShip, receiveAttack, shipsSunk,
   };
-  return { createGrid };
 };
 
 export { gameboard };
